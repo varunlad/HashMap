34e0b9444d6afaf6cfb6cf3d9c385bd898b4519a
varun <varunlad5@gmail.com>
2021-09-22 21:26:48 +0530

    [varun] Basic of Hash Map

---
+++ HashMap/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMapNode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(6);
+            hash.Add("1", "To");
+            hash.Add("2", "be");
+            hash.Add("3", "or");
+            hash.Add("4", "not");
+            hash.Add("5", "to");
+            //Console.WriteLine(hash.FrequencyOfWords("be"));
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value:" + hash5);
+            //hash.Remove("2");
+            string hash2 = hash.Get("2");
+            Console.WriteLine("2th index value : " + hash2);
+            Console.WriteLine(hash.FrequencyOfWords("be"));
+            Console.ReadLine();
+        }
+    }
+}
