f62939da9bfe196f40114d4b625b9f9a5f7fc52c
varun <varunlad5@gmail.com>
2021-09-27 07:52:02 +0530

    [varun] Removing a words from a sentence

--- HashMap/HashMap058.cs
+++ HashMap/HashMap058.cs
@@ -6,29 +6,77 @@ using System.Threading.Tasks;
 
 namespace MyMapNode
 {
-    public class MyMapNode<K, V>
+    public class MyMapNode<K, V> // K,V used because I have to build a Key Value in Hash Table.
     {
-        public struct KeyValues<p,q>
+        //this method is for passing Keyvalues in linkedlist k,v are data types
+        //Created a class for key,value pair.
+        public struct KeyValue<k, v>
         {
-            public K Key { get; set; }
-            public V Value { get; set; }
+            public k Key { get; set; }
+            public v Value { get; set; }
         }
         private readonly int size;
-        private readonly LinkedList <KeyValue<K, V>>[] items;
-        
+        private readonly LinkedList<KeyValue<K, V>>[] items;
 
+        //created a Constructor to initialize the variables.
         public MyMapNode(int size)
         {
-            this.size = size;
-            this.items = new LinkedList <KeyValue<K, V>>[size];
+            this.size = size; //this- is used to refer current class variables.
+            this.items = new LinkedList<KeyValue<K, V>>[size];
         }
-        protected int GetArrayPosition(K key)
+
+        //Method to find the position of the hash table(creating hash code)
+        protected int GetArrayPosition(K Key)
         {
-            int hash = key.GetHashCode();
-            int position = key.GetHashCode() % size;
+            int hash = Key.GetHashCode();
+
+            //%---> will give th mod value. So I used Math.Abs to find the interger of it.
+            int position = hash % size;
             return Math.Abs(position);
         }
-        public V Get(K key)
+
+        //method to get a value stored in particular key
+        // It is to Identify the array position at what Index particular key is located.
+        public V Get(K Key) // Its a Generic method.
+        {
+            int position = GetArrayPosition(Key);
+
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(Key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+
+        //method for to add values in hashtable
+        //It is just performing the operation.Iam just pushing the data into the hashtable using linked list operation.
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>()
+            //assign values to Key and Value
+            { Key = key, Value = value };
+            if (linkedList.Count != 0)
+            {
+                foreach (KeyValue<K, V> item1 in linkedList)
+                {
+                    if (item1.Key.Equals(key))
+                    {
+                        Remove(key);
+                        break;
+                    }
+                }
+            }
+            linkedList.AddLast(item);
+        }
+
+        //created a method to find the exist key in particular position.
+        public bool ExistKey(K key)
         {
             int position = GetArrayPosition(key);
             LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
@@ -36,46 +84,14 @@ namespace MyMapNode
             {
                 if (item.Key.Equals(key))
                 {
-                    return item.Value;
+                    return true;
                 }
             }
-            return default(V);
-        }
-        public void Add(K key, V value)
-        {
-            int position = GetArrayPosition(key);
-            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
-            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
-            linkedList.AddLast(item);
-        }
-        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
-        {
-            LinkedList<KeyValue<K, V>> linkedList = items[position];
-            if (linkedList == null)
-            {
-                linkedList = new LinkedList<KeyValue<K, V>>();
-                items[position] = linkedList;
-
-            }
-            return linkedList;
-
+            return false;
         }
 
-        public int FrequencyOfWords( V value)
-        {
-            int count = 0;
-            foreach (var linkedList in items)
-            {
-                foreach (KeyValue<K, V> item in linkedList)
-                {
-                    if(item.Value.Equals(value))
-                    {
-                        count = count + 1;
-                    }
-                }
-            }
-            return count;
-        }
+        //Method to remove  entry (value) from  a hashtable 
+        // It is just removing particular entry in the hashtable
         public void Remove(K key)
         {
             int position = GetArrayPosition(key);
@@ -87,20 +103,60 @@ namespace MyMapNode
                 if (item.Key.Equals(key))
                 {
                     itemFound = true;
+                    foundItem = item;
                 }
-                foundItem = item;
-
-            }
-            if (itemFound)
-            {
-                linkedList.Remove(foundItem);
+                if (itemFound)
+                {
+                    linkedList.Remove(foundItem);
+                    break;
+                }
             }
         }
+        //GetLinkedList method to create a linkedlist and store the values in that list. 
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
 
-    }
-    public struct KeyValue<k, v>
-    {
-        public k Key { get; set; }
-        public v Value { get; set; }
+        // CountFrequency method is to count the frequencies of word in hash table.
+        public void CountFrequency(string sentence)
+        {
+            MyMapNode<string, int> myHashTable = new MyMapNode<string, int>(10);
+            string[] words = sentence.ToLower().Split(' ');
+            foreach (string word in words)
+            {
+                if (myHashTable.ExistKey(word))
+                {
+                    myHashTable.Add(word, myHashTable.Get(word) + 1);
+                }
+                else
+                {
+                    myHashTable.Add(word, 1);
+                }
+            }
+            Console.WriteLine("Display frequencies of words");
+            myHashTable.Display();
+        }
+
+        //method to display the elements in the hash table.
+        public void Display()
+        {
+            foreach (var linkedList in items)
+            {
+                if (linkedList != null)
+                    foreach (var element in linkedList)
+                    {
+                        string res = element.ToString();
+                        if (res != null)
+                            Console.WriteLine(element.Key + " " + element.Value);
+                    }
+            }
+        }
     }
 }
--- HashMap/Program.cs
+++ HashMap/Program.cs
@@ -10,19 +10,31 @@ namespace MyMapNode
     {
         static void Main(string[] args)
         {
+
+            //calling a MyMapNode class.
             MyMapNode<string, string> hash = new MyMapNode<string, string>(6);
             hash.Add("1", "To");
             hash.Add("2", "be");
             hash.Add("3", "or");
             hash.Add("4", "not");
             hash.Add("5", "to");
-            //Console.WriteLine(hash.FrequencyOfWords("be"));
+            hash.Add("6", "be");
+            string hash6 = hash.Get("6");
+            Console.WriteLine("6th index value:" + hash6);
             string hash5 = hash.Get("5");
             Console.WriteLine("5th index value:" + hash5);
-            //hash.Remove("2");
+            string hash4 = hash.Get("4");
+            Console.WriteLine("4th index value:" + hash4);
+            string hash3 = hash.Get("3");
+            Console.WriteLine("3th index value:" + hash3);
+            hash.Remove("2");
             string hash2 = hash.Get("2");
             Console.WriteLine("2th index value : " + hash2);
-            Console.WriteLine(hash.FrequencyOfWords("be"));
+            string hash1 = hash.Get("1");
+            Console.WriteLine("1th index value : " + hash1);
+            //Calling Count Frequency Method.
+            hash.CountFrequency("To be or not to be");
+
             Console.ReadLine();
         }
     }
